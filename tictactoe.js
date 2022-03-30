e0780e9122574e409aef16941318aca5b7b47d80
SergioAnderson <sergioanderson918@gmail.com>
2022-03-29 19:13:24 -0700

    Initial commit

---
+++ tictactoe.js
@@ -0,0 +1,14 @@
+var cellone = document.querySelector("#one")
+var celltwo = document.querySelector("#two")
+var cellthree = document.querySelector("#three")
+var cellfour = document.querySelector("#four")
+var cellfive = document.querySelector("#five")
+var cellsix = document.querySelector("#six")
+var cellseven = document.querySelector("#seven")
+var celleight = document.querySelector("#eight")
+var cellnine = document.querySelector("#nine")
+var letters = ['X', 'O', '']
+
+cellone.addEventListener("click", function (){
+    cellone.textContent = "X"
+})
